3ff1fb17d56de2efa07dfc4c99a118f7c178c275
llorensvilla <llovigo@gmail.com>
2025-03-06 17:44:06 +0100

    clean

--- visualize_pockets_1.cmd
+++
@@ -1,7 +0,0 @@
-open 2lzm.pdb
-open predicted_pockets_1.pdb
-select :POC
-rep sphere sel
-color yellow sel
-~select
-focus
--- visualize_pockets_1.pml
+++
@@ -1,5 +0,0 @@
-load predicted_pockets_1.pdb
-show spheres, resn POC
-color yellow, resn POC
-set sphere_scale, 1.0
-zoom
